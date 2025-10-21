c08aa64e24bfd6e369580ec2618547703b1716a0
and910805 <goole910805@gmail.com>
2025-10-21 19:56:11 +0800

    Updates

--- src/components/Lightbox.jsx
+++
@@ -1,8 +0,0 @@
-export default function Lightbox({open, src, onClose}){
-  if(!open) return null
-  return (
-    <div className="fixed inset-0 bg-black/70 z-[100] flex items-center justify-center p-2" onClick={onClose}>
-      <img src={src} alt="lightbox" className="max-h-[85vh] max-w-[95vw] rounded-2xl border border-white/20" />
-    </div>
-  )
-}
--- src/data/photosEuro.js
+++
@@ -1,8 +0,0 @@
-import { assetUrl } from '../utils/assetUrl';
-
-export const photosEuro = [
-  assetUrl('photos/euro/木馬粉裙.jpg'),
-  assetUrl('photos/euro/白紗吊燈綠牆.jpg'),
-  assetUrl('photos/euro/白紗沙發.jpg'),
-  assetUrl('photos/euro/白紗棚拍.jpg'),
-];
